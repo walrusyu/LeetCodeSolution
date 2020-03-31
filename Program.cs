f4a1bd85d845829fb9d6fae004b74654bc91a68e
Yu Weifeng <yu.weifeng@bcrdteam.onmicrosoft.com>
2020-03-31 15:58:20 +0800

    fix bug

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace LeetCode
             var grid = new int[3][] { new int[] { 1, 3, 1 }, new int[] { 1, 5, 1 }, new int[] { 4, 2, 1 } };
             var words = new string[] { "What", "must", "be", "acknowledgment", "shall", "be" };
 
-            var result = solution.SimplifyPath2("/a/../../b/../c//.//");
+            var result = solution.SimplifyPath("/a/../../b/../c//.//");
             //var result = solution.MyPow(2.0, 10);
 
             Console.WriteLine(result);
